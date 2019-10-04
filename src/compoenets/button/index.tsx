429f90097c64133bc902716b5c3b006a9defc87c
Julio César <llulioscesar@gmail.com>
2019-10-04 14:25:14 -0500

    Boton

---
+++ src/compoenets/button/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button: React.FC<{text?:string}> = ({text}) => {
+    return (
+        <button>
+            {text}
+        </button>
+    )
+}
+
+export default Button
