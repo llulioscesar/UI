fad1c91b4e54faac25e8e598cdd607a31740b909
Julio Caicedo <llulioscesar@gamil.com>
2019-10-04 20:56:49 -0500

    Boton

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify-css', () => {
+    return gulp.src(['./src/**/*.css','./src/**/**/*.css'])
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./dist-css'));
+});
+
